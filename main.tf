a49a8062762f6efdd85ae86f09abb1f090107942
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-13 14:36:02 +0530

    s

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "instances" {
-  for_each  = var.component
+  for_each  = var.components
   source    = "git::https://github.com/Rajkumar221/tf-module-app.git"
   component = each.key
   env       = var.env
